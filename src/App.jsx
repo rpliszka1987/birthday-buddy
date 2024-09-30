45c64584d312e538d4434305abd38435360c226b
Robert Pliszka <robplisz@gmail.com>
2024-09-30 18:18:17 -0400

    Displayed items from the object data

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,18 @@ const App = () => {
   const [person, setPerson] = useState(data);
   return (
     <div>
-      <h2>Birthday Reminder - Starter</h2>
+      <h2>{person.length} Birthdays this Month</h2>
       {console.log(person)}
+      {person.map((people) => {
+        const {id, name, age, image} = people
+        return (
+          <div key={id}>
+            <img src={image} alt={name} style={{width: '10rem', height: '10rem'}}/>
+            <h2>{name}</h2>
+            <h3>{age}</h3>
+          </div>
+        )
+      })}
       
     </div>
     
